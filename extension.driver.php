69d4a4610edf650b84a4edd5ce8a9b2424570be0
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-04-27 13:26:48 -0400

    Update for Symphony 4.x
    Update release version.
    Update image path to take into account if JIT is installed or not.
    cc @nitriques

--- extension.driver.php
+++ extension.driver.php
@@ -8,7 +8,7 @@
 	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
 
 	Class extension_client_logo extends Extension {
-	
+
 		public function getSubscribedDelegates() {
 			return array(
 				array(
@@ -28,38 +28,48 @@
 				),
 			);
 		}
-	
+
 		public function uninstall(){
 			Administration::Configuration()->remove('client_logo');
 			Administration::Configuration()->saveConfig();
 		}
-		
+
 		//aggiungo il logo
 		public function initaliseAdminPageHead($context) {
 			$page = Administration::instance()->Page;
 			$pathlogo = General::Sanitize(Administration::Configuration()->get('path', 'client_logo'));
 			if($pathlogo != "" and file_exists(DOCROOT.'/workspace/'.$pathlogo)) {
+				$extman = Symphony::ExtensionManager();
+				$status = $extman->fetchStatus(array('handle' => 'jit_image_manipulation'));
+				$fullPathLogo = (in_array(EXTENSION_ENABLED, $status)) ? URL.'/image/2/0/70/5/'.$pathlogo : URL.'/workspace/'.$pathlogo;
+
 				//controllo l'esistenza e le misure del logo
 				$page->addElementToHead(new XMLElement("style", "
-					body #wrapper #header h1 {
-						position: relative;
-						padding: 10px 19px;
+					#header h1 a > span {
+						display: flex;
+						flex-direction: row;
+						align-items: center;
+						justify-content: flex-start;
 					}
-					body #wrapper #header h1 a {
+					#header h1 a > span:before {
 						display: block;
-						height: 70px;
-						background: url(" .URL.'/image/2/0/70/5/'.$pathlogo . ") no-repeat;
-						text-indent:-1000px;
-						border-bottom: none !important;
+						content: '';
+						margin-right: 1.5rem;
+						background-image: url(" . $fullPathLogo . ");
+						background-repeat: no-repeat;
+						background-position: center center;
+						background-size: contain;
+						width: 3rem;
+						height: 3rem;
 					}",
 					array(
 						"type" => "text/css",
 						"media" => "screen, projection")
-					), 
+					),
 					100012);
 			}
 		}
-	
+
 		//salvo le preferenze
 		public function savePreferences($context){
 			$pathlogo = $context['settings']['client_logo']['path'];
@@ -67,25 +77,25 @@
 				$context['settings']['client_logo'] = array("path" =>$pathlogo);
 			}
 		}
-	
-	
+
+
 		//imposto le preferenze
 		public function appendPreferences($context){
-	
+
 			$group = new XMLElement('fieldset');
 			$group->setAttribute('class', 'settings');
 			$group->appendChild(
 				new XMLElement('legend', 'Client logo')
 			);
-			
+
 			$pathlogo = Widget::Label(__('Client logo path (relative to workspace)'));
 			$pathlogo->appendChild(Widget::Input(
 				'settings[client_logo][path]', General::Sanitize(Administration::Configuration()->get('path', 'client_logo'))
 			));
 			$group->appendChild($pathlogo);
-			
+
 			$context['wrapper']->appendChild($group);
-			
+
 		}
-	
+
 	}
